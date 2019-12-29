6b96087a381c9acb2ccb120fd44f186e720b39a2
Franz Srambical <ownedbycreep@gmail.com>
2019-12-29 12:31:37 +0100

    Fehler teilweise korrigiert + bei Kap. 4 weitergeschrieben

---
+++ auto/kapitel1.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "kapitel1"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (LaTeX-add-labels
+    "Werte"))
+ :latex)
+
