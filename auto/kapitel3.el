543fa8ed608de1b7e39caa9233d43c122b27475e
Franz Srambical <ownedbycreep@gmail.com>
2020-01-01 17:06:50 +0100

    Reinforcement Learning (angefangen)

---
+++ auto/kapitel3.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "kapitel3"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "humanfeedbackimg"))
+ :latex)
+
