ab413fe1857ebdb6b0bf790c375adac69229ae51
Franz Srambical <ownedbycreep@gmail.com>
2020-01-03 18:16:44 +0100

    Mitten in "Deep Reinforcement Learning with human preference"

---
+++ auto/kapitel2.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "kapitel2"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "bösartigeKI"))
+ :latex)
+
--- auto/kapitel3.el
+++ auto/kapitel3.el
@@ -1,12 +1,13 @@
 (TeX-add-style-hook
  "kapitel3"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (LaTeX-add-labels
     "deeplearningimg"
+    "deepreinforcementlearningimg"
     "humanfeedbackimg"))
  :latex)
 
