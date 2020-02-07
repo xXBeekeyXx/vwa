e9b92b65afa3123203b373d977beca009e954e41
xXBeekeyXx <ownedbycreep@gmail.com>
2020-02-07 15:44:01 +0100

    Alles außer Einleitung, Schluss und Abstract

--- auto/document.el
+++ auto/document.el
@@ -5,21 +5,22 @@
                      '(("vwa" "DLS" "	inreferencehack" "	ohneVgl=false" "	ohneS=false" "	scauthor" "	rundeauslassung=false" "	bookstyle=false" "	widowlines=3" "	titlepage=DLS2017" "	listof=nochaptergap" "	doppelpunkt=false" "	postnotedoppelpunkt=false" "	zitierstil=klassisch")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("biblatex" "error" "backend=biber") ("siunitx" "output-decimal-marker={,}")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "abstract"
-    "vorwort"
     "einleitung"
     "kapitel1"
     "kapitel2"
     "kapitel3"
     "schluss"
     "literatur"
-    "anhang"
     "vwa"
     "vwa10"
     "biblatex"
--- auto/kapitel2.el
+++ auto/kapitel2.el
@@ -3,7 +3,10 @@
  (lambda ()
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "bösartigeKI"))
